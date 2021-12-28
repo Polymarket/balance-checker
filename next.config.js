e0c0140211f5b3e6b0892d2773228feec5d8775c
dylangolow <dylangolow@gmail.com>
2021-12-28 16:08:12 -0500

    add security headers, more stringent csp to come

--- next.config.js
+++ next.config.js
@@ -1,10 +1,41 @@
+const securityHeaders = [
+  {
+    // Added automatically if deploying to Vercel but kept for clarity
+    key: "Strict-Transport-Security",
+    value: "max-age=63072000; includeSubDomains; preload"
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block"
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN"
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff"
+  }
+  // To be added in follow up
+  // {
+  //     key: 'Content-Security-Policy',
+  //     value: "default-src 'self' *.polymarket.com *.poly.market;"
+  // }
+];
+
 module.exports = {
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ["@svgr/webpack"]
-      });
-  
-      return config;
-    }
-  };
+  webpack (config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  },
+  // async headers () {
+  //   return [{
+  //     source: "/(.*)",
+  //     headers: securityHeaders
+  //   }];
+  // }
+};
