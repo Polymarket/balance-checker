a27d3b5971b0b832d5fe27526579c1b90886a9cb
dylangolow <dylangolow@gmail.com>
2022-11-18 16:15:14 -0500

    chore: update _app.tsx for next 11.1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import "../styles/globals.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { AppProps } from "next/app";
+import type { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     return <Component {...pageProps} />;
